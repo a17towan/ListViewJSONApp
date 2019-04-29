347691062c8125f9e8703c03349a265288c9ddf7
a17towan <a17towan@student.his.se>
2019-04-29 13:47:26 +0200

    Copy pasted contents in mountain.java from the last ActivitiesApp assignment

--- app/src/main/java/com/example/brom/listviewjsonapp/Mountain.java
+++ app/src/main/java/com/example/brom/listviewjsonapp/Mountain.java
@@ -1,5 +1,28 @@
-package com.example.brom.listviewjsonapp;
+package com.example.brom.activitiesapp;
 
-public class Mountain {
-    
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MountainDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mountaindetails);
+
+        TextView test = (TextView) findViewById(R.id.test);
+        Intent intent = getIntent();
+        int height = intent.getIntExtra("HEIGHT", 0);
+        test.setText("The height is: " + height);
+
+        TextView test2 = (TextView) findViewById(R.id.test2);
+        String location = intent.getStringExtra("LOCATION");
+        test2.setText("The location is: " + location);
+
+        TextView test3 = (TextView) findViewById(R.id.test3);
+        String name = intent.getStringExtra("NAME");
+        test3.setText("The name is: " + name);
+    }
 }
