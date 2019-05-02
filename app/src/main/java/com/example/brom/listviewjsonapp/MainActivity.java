3b6475d11dfab53d30ac2647b0e77311c6b3f4eb
a17towan <a17towan@student.his.se>
2019-05-02 11:53:39 +0200

    Fixed so that you can refresh the data with a main_menu.xml file

--- app/src/main/java/com/example/brom/listviewjsonapp/MainActivity.java
+++ app/src/main/java/com/example/brom/listviewjsonapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -50,6 +52,12 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayAdapter<String> adapter;
     ListView my_listview;
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -177,6 +185,19 @@ public class MainActivity extends AppCompatActivity {
 
             */
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            adapter.clear();
+            new FetchData().execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
